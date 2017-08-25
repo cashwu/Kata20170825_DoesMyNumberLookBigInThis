9fa8e1f3c457124bf8566ac2e8f14ba5c33c4ecd
cash <cash@cashwu.com>
2017-08-25 22:27:23 +0800

    input 1 test case

--- Kata20170825_DoesMyNumberLookBigInThis/NarcissisticTests.cs
+++ Kata20170825_DoesMyNumberLookBigInThis/NarcissisticTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170825_DoesMyNumberLookBigInThis
     public class NarcissisticTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_1_should_return_true()
         {
+            NarcissisticShouldBeTrue(1);
+        }
+
+        private static void NarcissisticShouldBeTrue(int value)
+        {
+            var kata = new Kata();
+            var actual = kata.Narcissistic(value);
+            Assert.IsTrue(actual);
+        }
+    }
+
+    public class Kata
+    {
+        public bool Narcissistic(int value)
+        {
+            return true;
         }
     }
 }
